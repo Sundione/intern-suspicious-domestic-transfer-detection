ff5b364ab4a46d0f774a86b4cffe97028bf11dee
Tawan <tawan.t@t2pco.com>
2025-11-18 11:03:29 +0700

    demonstrate adapter (gemini)

---
+++ adapter/genai/model/exception.py
@@ -0,0 +1,14 @@
+class InvalidGenAIClientResponse(Exception):
+    pass
+
+
+class InvalidGenAIInput(Exception):
+    pass
+
+
+class InvalidCacheInput(Exception):
+    pass
+
+
+class InvalidGenAIConfiguration(Exception):
+    pass
